511c3563130a260c13d560ff68dcb09bc7caa486
qtuancr261 <tuancr261@gmail.com>
2018-06-12 11:05:04 +0700

    Refactor source code 	 - use VideoCapture instead of CvCapture          - Remove unused unique_ptr<IBGS> bgs, IplImage* frame

--- Demo.cpp
+++ Demo.cpp
@@ -1,11 +1,12 @@
-#include "package_analysis/ObjectCouting.h"
-#include "package_bgs/PBAS/PixelBasedAdaptiveSegmenter.h"
-#include "package_tracking/BlobTracking.h"
+#include "analysis_algorithms/ObjectCouting.h"
+#include "backgroundsub_algorithms/PBAS/PixelBasedAdaptiveSegmenter.h"
+#include "tracking_algorithms/BlobTracking.h"
 #include <iostream>
 #include <memory>
 #include <opencv2/bgsegm.hpp>
 #include <opencv2/opencv.hpp>
 using cv::BackgroundSubtractor;
+using cv::VideoCapture;
 using cv::Ptr;
 using cv::bgsegm::BackgroundSubtractorMOG;
 using cv::bgsegm::createBackgroundSubtractorMOG;
@@ -18,15 +19,16 @@ int main(int argc, char* argv[])
     std::cout << "Using OpenCV " << CV_MAJOR_VERSION << "." << CV_MINOR_VERSION << "." << CV_SUBMINOR_VERSION << std::endl;
 
     /* Open video file */
-    CvCapture* capture = 0;
-    capture = cvCaptureFromAVI(argv[1]);
+    VideoCapture systemCapture;
+    systemCapture.open(argv[1]);
+    /*capture = cvCaptureFromAVI(argv[1]);
     if (!capture) {
         std::cerr << "Cannot open video!" << std::endl;
         return 1;
-    }
+    }*/
 
     /* Background Subtraction Algorithm */
-    unique_ptr<IBGS> bgs{ make_unique<PixelBasedAdaptiveSegmenter>() };
+    //unique_ptr<IBGS> bgs{ make_unique<PixelBasedAdaptiveSegmenter>() };
     /* Blob Tracking Algorithm */
     cv::Mat img_blob;
     unique_ptr<BlobTracking> blobTracking{ make_unique<BlobTracking>() };
@@ -35,19 +37,19 @@ int main(int argc, char* argv[])
     unique_ptr<ObjectCouting> vehicleCouting{ make_unique<ObjectCouting>() };
 
     std::cout << "Press 'q' to quit..." << std::endl;
-    IplImage* frame;
 
-    Ptr<BackgroundSubtractorMOG> backgroundSuctractor;
-    backgroundSuctractor = createBackgroundSubtractorMOG(200, 5, 0.7, 15);
-
+    Ptr<BackgroundSubtractorMOG> backgroundSuctractor{createBackgroundSubtractorMOG(200, 5, 0.7, 15)};
+    Mat img_input{};
     while (1)
     {
-        frame = cvQueryFrame(capture);
-        if (!frame)
+        //frame = cvQueryFrame(capture);
+        //if (!frame)
+        //    break;
+        systemCapture >> img_input;
+        if (img_input.empty())
             break;
-
-        cv::Mat img_input = cv::cvarrToMat(frame);
-        cv::imshow("Input", img_input);
+        //cv::Mat img_input = cv::cvarrToMat(frame);
+        //cv::imshow("Input", img_input);
 
         // bgs->process(...) internally process and show the foreground mask image
         cv::Mat img_output;
@@ -70,7 +72,7 @@ int main(int argc, char* argv[])
     }
 
     cvDestroyAllWindows();
-    cvReleaseCapture(&capture);
+    //cvReleaseCapture(&capture);
 
     return 0;
 }
